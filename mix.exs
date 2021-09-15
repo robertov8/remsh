507c9976b924d3fd0400ad6626e8f1469f96806c
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-14 22:30:20 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Remsh.MixProject do
   def project do
     [
       app: :remsh,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
