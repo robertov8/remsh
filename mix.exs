ee99ddff9ba6c4b6e0c273e999468cff472a8885
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-14 22:20:45 -0300

    support old versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Remsh.MixProject do
     [
       app: :remsh,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
